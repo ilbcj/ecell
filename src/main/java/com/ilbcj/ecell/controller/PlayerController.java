eee6d784c3f53959737d87279ed0ff154a002218
ilbcj <ilbcj@hotmail.com>
2020-05-20 17:13:22 +0800

    更新选手controller
    去除无用包引用

--- src/main/java/com/ilbcj/ecell/controller/PlayerController.java
+++ src/main/java/com/ilbcj/ecell/controller/PlayerController.java
@@ -7,11 +7,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
