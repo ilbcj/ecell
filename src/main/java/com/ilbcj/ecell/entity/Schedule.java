38b66bbd61a4eba95238a3248800ab52f39be453
ilbcj <ilbcj@hotmail.com>
2020-05-26 17:46:29 +0800

    修改赛程页记录信息颜色

--- src/main/java/com/ilbcj/ecell/entity/Schedule.java
+++ src/main/java/com/ilbcj/ecell/entity/Schedule.java
@@ -20,31 +20,31 @@ public class Schedule implements Serializable, Comparable<Schedule>{
 	public static final Integer TYPE_REGULAR = 1;
 	public static final Integer TYPE_PLAYOFF  = 2;
 	
-	public static final String ROUND_R_1 = "常规赛16进8_1";
-	public static final String ROUND_R_2 = "常规赛16进8_2";
-	public static final String ROUND_R_3 = "常规赛16进8_3";
-	public static final String ROUND_R_4 = "常规赛16进8_4";
+	public static final String ROUND_R_1 = "常规赛16进8_A组";
+	public static final String ROUND_R_2 = "常规赛16进8_B组";
+	public static final String ROUND_R_3 = "常规赛16进8_C组";
+	public static final String ROUND_R_4 = "常规赛16进8_D组";
 	public static final String ROUND_R_5 = "常规赛8进4_1";
 	public static final String ROUND_R_6 = "常规赛8进4_2";
 	public static final String ROUND_R_7 = "常规赛8进4_3";
 	public static final String ROUND_R_8 = "常规赛8进4_4";
 	public static final String ROUND_R_9 = "常规赛4进2_1";
 	public static final String ROUND_R_10 = "常规赛4进2_2";
-	public static final String ROUND_R_11 = "常规赛34名";;
-	public static final String ROUND_R_12 = "常规赛12名";
+	public static final String ROUND_R_11 = "常规赛3、4名";;
+	public static final String ROUND_R_12 = "常规赛1、2名";
 	
-	public static final String ROUND_P_1 = "季后赛16进8_1";
-	public static final String ROUND_P_2 = "季后赛16进8_2";
-	public static final String ROUND_P_3 = "季后赛16进8_3";
-	public static final String ROUND_P_4 = "季后赛16进8_4";
+	public static final String ROUND_P_1 = "季后赛16进8_A组";
+	public static final String ROUND_P_2 = "季后赛16进8_B组";
+	public static final String ROUND_P_3 = "季后赛16进8_C组";
+	public static final String ROUND_P_4 = "季后赛16进8_D组";
 	public static final String ROUND_P_5 = "季后赛8进4_1";
 	public static final String ROUND_P_6 = "季后赛8进4_2";
 	public static final String ROUND_P_7 = "季后赛8进4_3";
 	public static final String ROUND_P_8 = "季后赛8进4_4";
 	public static final String ROUND_P_9 = "季后赛4进2_1";
 	public static final String ROUND_P_10 = "季后赛4进2_2";
-	public static final String ROUND_P_11 = "季后赛34名";;
-	public static final String ROUND_P_12 = "季后赛12名";
+	public static final String ROUND_P_11 = "季后赛3、4名";;
+	public static final String ROUND_P_12 = "季后赛1、2名";
 	
 	public static final Integer FORMAT_BO3 = 3;
 	public static final Integer FORMAT_BO5 = 5;
